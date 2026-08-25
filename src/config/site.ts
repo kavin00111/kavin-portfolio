230bac83d6ace09ed2bb696ce1cd0b033c88f62d
kavin.npc <166320340+kavin00111@users.noreply.github.com>
2026-08-25 20:15:23 +0530

    Update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -50,8 +50,8 @@ export const siteConfig = {
   links: {
     email: 'Developerkavinxx@gmail.com',
     github: 'https://github.com/kavin00111',
-    linkedin: 'https://www.linkedin.com/in/kavin-chaudhary-7b7392306?utm_source=share_via&utm_content=profile&utm_medium=member_android',
-    instagram: 'https://www.instagram.com/kvinkx7.npc?igsi=MWgyMHJjam56ZG5tcg==',
+    linkedin: 'https://www.linkedin.com/in/kavin-chaudhary-7b7392306',
+    instagram: 'https://www.instagram.com/kvinkx7.npc',
   },
 
   // GitHub API username used to pull live repo/activity data in the
